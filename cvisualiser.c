dc84b227e124b972ca18a4997e5676fed77e5f27
GWStuart <89712016+GWStuart@users.noreply.github.com>
2025-07-15 23:20:35 +1000

    added message rendering

--- cvisualiser.c
+++ cvisualiser.c
@@ -18,10 +18,18 @@
 #define ZOOM_DIFF ZOOM_FACTOR - 1
 #define INIT_SCALE WIDTH
 
+// other constants
+#define PADDING 5
+#define FONT_SIZE 18
+
+// define string constants
+const char* const MSG_START = "Select a starting point";
+
 // define SDL colours
 SDL_Color BG = {0x28, 0x28, 0x28, 0xff};
-SDL_FColor WHITE = {0xff, 0xff, 0xff, 0xff};
+SDL_Color WHITE = {0xff, 0xff, 0xff, 0xff};
 SDL_Color RED = {0xff, 0x00, 0x00, 0xff};
+SDL_Color BLUE = {0x70, 0xd6, 0xff, 0xff};
 
 typedef struct {
     double x;
@@ -74,7 +82,7 @@ void draw_road(SDL_Renderer* renderer, Camera camera, Road road) {
 }
 
 void render_screen(Display display, Node* nodes, int numNodes, Road* roads, 
-        int numRoads, char* fpsText) {
+        int numRoads, SDL_Texture* message, char* fpsText) {
     // fill screen
     SDL_SetRenderDrawColor(display.renderer, BG.r, BG.g, BG.b, BG.a);
     SDL_RenderClear(display.renderer);
@@ -87,7 +95,7 @@ void render_screen(Display display, Node* nodes, int numNodes, Road* roads,
 
     // render the nodes
     if (display.showNodes) {
-        SDL_SetRenderDrawColor(display.renderer, RED.r, RED.g, RED.b, RED.a);
+        SDL_SetRenderDrawColor(display.renderer, BLUE.r, BLUE.g, BLUE.b, BLUE.a);
         for (int i=0; i<numNodes; i++) {
             draw_point(display.renderer, *display.camera, nodes[i].pos);
         }
@@ -100,8 +108,25 @@ void render_screen(Display display, Node* nodes, int numNodes, Road* roads,
     SDL_SetRenderDrawColor(display.renderer, WHITE.r, WHITE.g, WHITE.b, WHITE.a);
     SDL_RenderDebugText(display.renderer, 5, 5, fpsText);
 
+    // render the message
+    rect = (SDL_FRect){0, HEIGHT - message->h, message->w + PADDING*2, 
+                       message->h};
+    SDL_SetRenderDrawColor(display.renderer, BLUE.r, BLUE.g, BLUE.b, BLUE.a);
+    SDL_RenderFillRect(display.renderer, &rect);
+    rect = (SDL_FRect){PADDING, HEIGHT - message->h, message->w, message->h};
+    SDL_RenderTexture(display.renderer, message, NULL, &rect);
+
     // update the display
     SDL_RenderPresent(display.renderer);
+}
+
+SDL_Texture* create_message(SDL_Renderer* renderer, TTF_Font* font, 
+        const char* message) {
+    SDL_Surface* surface = TTF_RenderText_Blended(font, message, 
+            strlen(message), BG);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_DestroySurface(surface);
+    return texture;
 }
 
 int main() {
@@ -116,18 +141,24 @@ int main() {
     //int numRoads = load_roads("assets/data/RomeFull.roads", nodes, &roads);
     int numRoads = load_roads("assets/data/Brisbane.roads", nodes, &roads);
 
-    // initialise the SDL window
+    // initialise SDL
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
+
+    // setup window
     SDL_Window* window = SDL_CreateWindow("Pathfinder", WIDTH, HEIGHT, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
     //SDL_SetWindowFullscreen(window, true); // uncomment for fullscreen
 
     // setup fonts
-    //TTF_Font* font = TTF_OpenFont("assets/fonts/DejaVuSans.ttf", 20);
+    TTF_Font* font = TTF_OpenFont("assets/fonts/DejaVuSans.ttf", FONT_SIZE);
+    SDL_Texture* message = create_message(renderer, font, MSG_START);
 
-    // Initialise the camera
+    // initialise the camera
     Camera camera = (Camera){0, 0, 1};
+
+    // create the display struct
+    Display display = (Display){renderer, &camera, false, true};
 
     // keep track of FPS count
     Uint64 now = SDL_GetPerformanceCounter();
@@ -137,9 +168,6 @@ int main() {
     double fpsTimer = 0;
     int frameCounter = 0;
     char fpsText[16];
-
-    // move this later
-    Display display = (Display){renderer, &camera, false, true};
 
     SDL_Event event;
     bool mouseDown = false; // keep track of mouse state
@@ -211,6 +239,7 @@ int main() {
             frameCounter = 0;
         }
 
-        render_screen(display, nodes, numNodes, roads, numRoads, fpsText);
+        render_screen(display, nodes, numNodes, roads, numRoads, message, 
+                fpsText);
     }
 }
